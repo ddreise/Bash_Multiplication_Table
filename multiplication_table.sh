7f59b00a92d1d674c34c1839ec593090234a660b
ddreise <ddreise@gmail.com>
2019-10-20 00:30:59 -0400

    Completed

--- multiplication_table.sh
+++ multiplication_table.sh
@@ -1,4 +1,4 @@
-#!/usr/local/Cellar/bash/5.0.11/bin/bash
+#!/bin/bash
 #
 # Question 3 - Multipliation Table
 # Operating Systems Midterm Exam
@@ -13,41 +13,65 @@
 
 # Main menu
 
+echo
+echo "************** Multiplication Table **************"
+echo
+echo "This program multiplies a user given number with" 
+echo "numbers between 0 and 10. The user will be"
+echo "prompted to input two variables. The first is the"
+echo "number they wish to multiply, and the second is "
+echo "the filename they wish to output it to."
+echo
+echo "An error code will display if user inputs"
+echo "incorrectly."
+echo
+echo "Type \"Quit\" to exit program."
+echo
+echo "**************************************************"
+echo
 
 
-while [ "$usr_input" != "Quit" ]; do
+# Infinite loop until user exits
+while true 
+do
 
-    #echo ${BASH_VERSION}
     echo 
-    echo -n "Enter a multiplier and filename (separated by a space): "
+    echo -n "Enter a multiplier and filename (Format: MULTIPLIER FILENAME): "
 
-    read -a usr_input     # wait for user input
 
-    if [ ${usr_input[0]} = 'Quit' ]
+    read -a usr_input                   # wait for user input, turn string into array separatad by spaces with "-a"
+
+    
+    # if user types "Quit", exit program
+    if [ ${usr_input[0]} = 'Quit' ]     
     then
         exit
     fi
 
-    #echo "first word ${usr_input[0]} "
-    #echo "second word ${usr_input[1]} "
+    # if user does not input exactly 2 values, exit with error message
+    if [ ${#usr_input[@]} != 2 ]        
+    then
+        echo "E: Need a multiplier and a file name to invoke this program properly!"
+        exit 1
+    fi
 
 
+    # loop 10 times multiplying user chosen value by incrementing value
     for (( i = 0; i <= 10; i++ ));
     do
+        # Multiply incrementing value by user chosen value
         a=$(( i*${usr_input[0]} ))
-        echo "$a" >> ${usr_input[1]}
+
+        # Output new table to file. Exit with error code if file was unable to be opened.
+        echo "$a" >> ${usr_input[1]} || { echo "E: Failed to open file"; exit 2; }
 
     done
 
-    cat ${usr_input[1]}
+
+    cat ${usr_input[1]}         # output file contents to terminal
     
-    rm ${usr_input[1]}
 
-
-
-
-
-
+    rm ${usr_input[1]}          # delete created file
 
 
 done
